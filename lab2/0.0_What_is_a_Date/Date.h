effaacbf8f44e1d7d496b1a4956c2e9255bb1c0e
TobiasLundin <tobbesayssend2me@hotmail.com>
2015-10-16 10:46:53 +0200

    start på labb2

---
+++ lab2/0.0_What_is_a_Date/Date.h
@@ -0,0 +1,20 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include "kattistime.h"
+class Date
+{
+	public:
+
+	// A default-constructed
+	// object shall contain a date that refers to the current day (i.e. the day when
+	// the program is executed)
+		Date(){
+		
+		}
+
+};
+
+
+#endif  
